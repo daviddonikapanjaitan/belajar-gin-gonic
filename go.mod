fdbd687c115652bbb2482b900b8fcc5f23a23ea0
daviddonikapanjaitan <daviddonikapanjaitan@gmail.com>
2021-07-22 09:32:20 +0700

    video tiga

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/daviddonikapanjaitan/belajar-gin-gonic
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.2 // indirect
+require (
+	github.com/gin-gonic/gin v1.7.2 // indirect
+	github.com/go-playground/validator v9.31.0+incompatible
+	github.com/go-playground/validator/v10 v10.7.0
+	github.com/tpkeeper/gin-dump v1.0.0
+)
--- server.go
+++ server.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"io"
+	"net/http"
+	"os"
+
 	"github.com/daviddonikapanjaitan/belajar-gin-gonic/controller"
+	"github.com/daviddonikapanjaitan/belajar-gin-gonic/middlewares"
 	"github.com/daviddonikapanjaitan/belajar-gin-gonic/service"
 	"github.com/gin-gonic/gin"
+	gindump "github.com/tpkeeper/gin-dump"
 )
 
 var (
@@ -11,21 +17,31 @@ var (
 	videoController controller.VideoController = controller.New(videoService)
 )
 
-func main() {
-	server := gin.Default()
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
 
-	server.GET("/test", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "OK!!",
-		})
-	})
+func main() {
+
+	setupLogOutput()
+
+	server := gin.New()
+
+	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth(), gindump.Dump())
 
 	server.GET("/videos", func(ctx *gin.Context) {
 		ctx.JSON(200, videoController.FindAll())
 	})
 
 	server.POST("/videos", func(ctx *gin.Context) {
-		ctx.JSON(200, videoController.Save(ctx))
+		err := videoController.Save(ctx)
+
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		} else {
+			ctx.JSON(http.StatusOK, gin.H{"message": "Video Input is Valid!!"})
+		}
 	})
 
 	server.Run(":8080")
